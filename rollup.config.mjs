04e7707d4dbf25a46e14ab3c9052a4c43c32ea42
Hidayt Rahman <hidaytrahman@gmail.com>
2023-02-27 17:15:09 +0530

    feat(config): ui scss support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 // import packageJson from "./package.json" assert { type: "json" };
+import scss from "rollup-plugin-scss";
 
 export default [
   {
@@ -23,11 +24,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      scss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.(css|less|scss)$/],
   },
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./button.scss";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = ({ label }: ButtonProps) => {
-  return <button>{label}</button>;
+  return <button className="btn">{label}</button>;
 };
 
 export default Button;
