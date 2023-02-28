3781d751d24537abd544b95dbf8399b712ffa8b6
Hidayt Rahman <hidaytrahman@gmail.com>
2023-03-01 01:22:14 +0530

    chore(support): ref forwardRef soppprt for input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { InputContainer } from "./Input.styles";
 import { InputProps } from "./types";
 
-const Input = ({
-  variant = "primary",
-  scale,
-  fullWidth,
-  ...props
-}: InputProps) => {
-  return (
-    <InputContainer
-      variant={variant}
-      scale={scale}
-      fullWidth={fullWidth}
-      {...props}
-    />
-  );
-};
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { variant = "primary", scale = "regular", fullWidth = false, ...props },
+    ref
+  ) => {
+    return (
+      <InputContainer
+        variant={variant}
+        scale={scale}
+        fullWidth={fullWidth}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Input;
