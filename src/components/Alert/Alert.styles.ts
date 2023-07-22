ba703e2b0d49b6a97f9e33435cc08ccb4bfc8bab
Hidayt Rahman <hidaytrahman@gmail.com>
2023-07-22 23:53:52 +0530

    color changes for alert

--- src/components/Alert/Alert.styles.ts
+++ src/components/Alert/Alert.styles.ts
@@ -1,37 +1,37 @@
-import styled from "styled-components";
-import { AlertSize, AlertVariant } from "./types";
+import styled from 'styled-components';
+import { AlertSize, AlertVariant } from './types';
 
 interface AlertContainerTypes {
-  variant: AlertVariant;
-  size: AlertSize;
+	variant: AlertVariant;
+	size: AlertSize;
 }
 
 export const AlertContainer = styled.div<AlertContainerTypes>`
-  padding: 8px 10px;
-  border: 0;
-  border-radius: 5px;
+	padding: 8px 10px;
+	border: 0;
+	border-radius: 5px;
 
-  ${({ variant }) =>
-    variant === "primary"
-      ? `background: #D9E4DD;`
-      : variant === "secondary"
-      ? `background: #FBF7F0;`
-      : variant === "danger"
-      ? `background: red;`
-      : variant === "warning"
-      ? `background: yellow;`
-      : variant === "success"
-      ? `background: green;`
-      : `color: #000;`};
+	${({ variant }) =>
+		variant === 'primary'
+			? `background: #D9E4DD;`
+			: variant === 'secondary'
+			? `background: #FBF7F0;`
+			: variant === 'danger'
+			? `background: #8C3333;`
+			: variant === 'warning'
+			? `background: #F2EE9D;`
+			: variant === 'success'
+			? `background: #7A9D54;`
+			: `color: #000;`};
 
-  ${({ size }) =>
-    size === "xlarge"
-      ? `font-size: 22px; padding: 12px 20px;`
-      : size === "large"
-      ? `font-size: 18px;  padding: 10px 18px;`
-      : size === "small"
-      ? `font-size: 12px;`
-      : size === "xsmall"
-      ? `font-size: 10px;`
-      : `font-size: 14px;`};
+	${({ size }) =>
+		size === 'xlarge'
+			? `font-size: 22px; padding: 12px 20px;`
+			: size === 'large'
+			? `font-size: 18px;  padding: 10px 18px;`
+			: size === 'small'
+			? `font-size: 12px;`
+			: size === 'xsmall'
+			? `font-size: 10px;`
+			: `font-size: 14px;`};
 `;
