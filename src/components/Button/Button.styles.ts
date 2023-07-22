ca54ca5ffee5f8e8366b34b5a9b0388ac4a80063
Hidayt Rahman <hidaytrahman@gmail.com>
2023-07-22 23:44:55 +0530

    added hover effects for button

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,38 +1,42 @@
-import styled from "styled-components";
-import { ButtonSize, ButtonVariant } from "./types";
+import styled from 'styled-components';
+import { ButtonSize, ButtonVariant } from './types';
 
 interface ButtonContainerTypes {
-  variant: ButtonVariant;
-  size: ButtonSize;
+	variant: ButtonVariant;
+	size: ButtonSize;
 }
 
 export const ButtonContainer = styled.button<ButtonContainerTypes>`
-  padding: 8px 10px;
-  border: 0;
-  cursor: pointer;
-  border-radius: 5px;
+	padding: 8px 10px;
+	border: 0;
+	cursor: pointer;
+	border-radius: 5px;
 
-  ${({ variant }) =>
-    variant === "primary"
-      ? `background: #D9E4DD;`
-      : variant === "secondary"
-      ? `background: #FBF7F0;`
-      : variant === "danger"
-      ? `background: red;`
-      : variant === "warning"
-      ? `background: yellow;`
-      : variant === "link"
-      ? `background: transparent;`
-      : `color: #000;`};
+	&:hover {
+		opacity: 0.6;
+	}
 
-  ${({ size }) =>
-    size === "xlarge"
-      ? `font-size: 22px; padding: 12px 20px;`
-      : size === "large"
-      ? `font-size: 18px;  padding: 10px 18px;`
-      : size === "small"
-      ? `font-size: 12px;`
-      : size === "xsmall"
-      ? `font-size: 10px;`
-      : `font-size: 14px;`};
+	${({ variant }) =>
+		variant === 'primary'
+			? `background: #D9E4DD;`
+			: variant === 'secondary'
+			? `background: #FBF7F0;`
+			: variant === 'danger'
+			? `background: red;`
+			: variant === 'warning'
+			? `background: yellow;`
+			: variant === 'link'
+			? `background: transparent;`
+			: `color: #000;`};
+
+	${({ size }) =>
+		size === 'xlarge'
+			? `font-size: 22px; padding: 12px 20px;`
+			: size === 'large'
+			? `font-size: 18px;  padding: 10px 18px;`
+			: size === 'small'
+			? `font-size: 12px;`
+			: size === 'xsmall'
+			? `font-size: 10px;`
+			: `font-size: 14px;`};
 `;
